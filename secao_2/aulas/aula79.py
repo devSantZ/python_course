249fa1af024522762c4000b8df7cb3115b6370d0
Diego Santos <dieboas88@gmail.com>
2023-05-23 14:51:06 -0300

    seção 2

---
+++ secao_2/aulas/aula79.py
@@ -0,0 +1,15 @@
+"""
+Introdução à List comprehension em Python
+List comprehension é uma forma rápida para criar listas
+a partir de iteráveis.
+"""
+lista = [i for i in range(10)]
+print(lista)
+
+
+def mult(x=1, y = 0):
+    valores = [i*x for i in range(y)]
+    return valores
+
+teste = mult(3, 10)
+print(teste)
