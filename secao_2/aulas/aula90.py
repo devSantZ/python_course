2bc31d2a944d30e8058a9f45a66a0b7f1d061a2b
Diego Santos <dieboas88@gmail.com>
2023-05-30 12:59:48 -0300

    seção 2

---
+++ secao_2/aulas/aula90.py
@@ -0,0 +1,49 @@
+"""
+iterables e iterators
+
+iteravel: Um objeto iterável é aquele que pode ser percorrido,
+    ou seja, você pode obter um iterador a partir dele. Em termos 
+    simples, um iterável é uma sequência de elementos que pode ser
+    iterada (percorrida) um a um. Exemplos comuns de iteráveis em 
+    Python incluem listas, tuplas, strings, dicionários e conjunto.
+    
+iterador: Um iterador é um objeto que representa um estado de 
+    iteração sobre um iterável. Ele mantém uma referência para o 
+    elemento atual em uma sequência e sabe como acessar o próximo 
+    elemento. Em Python, um iterador é implementado por meio 
+    de dois métodos: __iter__() e __next__(). O método __iter__() 
+    retorna o próprio objeto iterador, enquanto o método 
+    __next__() retorna o próximo elemento da sequência.
+"""
+
+
+def iterador(x):
+    iterador_x = iter(x)
+    while x:
+        try:
+            print(next(iterador_x))
+            continue
+        except StopIteration:
+            return
+    
+    
+lista = [ 1, 2, 3, 4, 5, 6]
+tupla = ('Diego', 3, 5, True, 2.4, 'Python')
+dicionario = {
+    'nome': 'Lucas',
+    'sobrenome': 'Santos',
+    'idade': 32,
+    'endereco': [{
+        'rua1': 'Nomer1',
+        'rua2': 'Nomer2'
+        }]
+}
+
+teste = iterador(lista)
+print(teste)
+
+teste2 = iterador(tupla)
+print(teste2)
+
+teste3 = iterador(dicionario)
+print(teste3)
