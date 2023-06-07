305874447808b39910ea817998ac80ebdb737a73
Diego Santos <dieboas88@gmail.com>
2023-06-07 14:20:07 -0300

    pacote da aula99.py

---
+++ secao_2/aulas/aula99_package/modulo.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'variavel',
+] # --> apenas o que estiver dentro dessa lista será impotado
+# caso o usuário use o import *
+
+variavel = 'teste'
+
+def func_do_modulo(x, y):
+    return x + y
