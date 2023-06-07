7eb02d8373b7103f12923f1e599ee6f4ec57cea8
Diego Santos <dieboas88@gmail.com>
2023-06-07 13:29:21 -0300

    seção 2

--- secao_2/aulas/aula97.py
+++ secao_2/aulas/aula97.py
@@ -1,13 +1,28 @@
-import sys
+"""
+Modularização - Entendendo os seus próprios módulos Python
+O primeiro módulo executado chama-se __main__
+Você pode importar outro módulo inteiro ou parte do módulo
+O python conhece a pasta onde o __main__ está e as pastas
+abaixo dele.
+Ele não reconhece pastas e módulos acima do __main__ por
+padrão
+O python conhece todos os módulos e pacotes presentes
+nos caminhos de sys.path
+"""
+# import sys
 
 
-try:
-    sys.path.append('/home/devsantz/Downloads') # aplique o caminho do seu computador
-    import aula97_modulo
+# try:
+#     sys.path.append('secao_2/aulas/aula97_modulo.py') # aplique o caminho do seu computador
+#     from aula97_modulo import varialvel
+#     print(varialvel)
     
-except ModuleNotFoundError as e:
-    print(f'Módulo não encontrado, erro {e}')
+# except ModuleNotFoundError as e:
+#     print(f'Módulo não encontrado, erro {e}')
     
 
 
-print(*sys.path, sep='\n')
+# print(*sys.path, sep='\n')
+import aula97_modulo
+
+print('Este módulo se chama', __name__)
