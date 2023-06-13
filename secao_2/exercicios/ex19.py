56980b3edda14c91645392ec73745635ffb43fc2
Diego Santos <dieboas88@gmail.com>
2023-06-13 12:26:38 -0300

    seção 2

---
+++ secao_2/exercicios/ex19.py
@@ -0,0 +1,23 @@
+"""
+Adiantamento de Funções
+"""
+def soma(x, y):
+    return x + y
+
+
+def divide(x, y):
+    return x / y
+
+
+def exec(func, y):
+    def interna(x):
+        return func(x, y)
+    return interna
+
+
+
+soma_com_cinco = exec(soma, 5)
+divide_por_2 = exec(divide, 2)
+
+print(soma_com_cinco(2))
+print(divide_por_2(10))
