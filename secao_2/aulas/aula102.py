093f2228d6cbfb9de4338240c6fe52d3124e63cf
devsantz <devsantz@pop-os.localdomain>
2023-06-26 08:51:13 -0300

    seção 2

--- secao_2/aulas/aula102.py
+++ secao_2/aulas/aula102.py
@@ -3,6 +3,7 @@ funções decoradoras com parâmetro
 """
 # funcção decoradora
 
+
 def fabrica_de_decoradores(a=None, b=None, c=None):
     def decoradora(func):
         def aninhada(*args, **kwargs):
@@ -16,6 +17,7 @@ def fabrica_de_decoradores(a=None, b=None, c=None):
 def soma(n=None):
     return n
 
+
 # Exemplo
 def imposto_em_porcentagem(x):
     def imposto(func):
@@ -26,9 +28,11 @@ def imposto_em_porcentagem(x):
         return inner_function
     return imposto
 
+
 @imposto_em_porcentagem(0.1)
 def soma(x, y):
     return x + y
+
 
 print(soma(5, 5))
 
