d511ea5dd7a524c0c1b6c25d18286cbe051fbe9e
devsantz <devsantz@pop-os.localdomain>
2023-07-09 15:24:30 -0300

    seção 2

--- secao_2/aulas/aula114.py
+++ secao_2/aulas/aula114.py
@@ -4,8 +4,8 @@
 import json
 
 pessoa = {
-    'nome': 'Luiz Otávio 2',
-    'sobrenome': 'Miranda',
+    'nome': 'Diego ',
+    'sobrenome': 'Santos    ',
     'enderecos': [
         {'rua': 'R1', 'numero': 32},
         {'rua': 'R2', 'numero': 55},
@@ -16,7 +16,7 @@ pessoa = {
     'nada': None,
 }
 
-with open('aula117.json', 'w', encoding='utf8') as arquivo:
+with open('aula114.json', 'w', encoding='utf8') as arquivo:
     json.dump(
         pessoa,
         arquivo,
@@ -24,8 +24,8 @@ with open('aula117.json', 'w', encoding='utf8') as arquivo:
         indent=2,
     )
 
-with open('aula117.json', 'r', encoding='utf8') as arquivo:
+with open('aula114.json', 'r', encoding='utf8') as arquivo:
     pessoa = json.load(arquivo)
     # print(pessoa)
     # print(type(pessoa))
-    print(pessoa['nome'])
+    print(pessoa)
--- secao_2/aulas/aula115.py
+++ secao_2/aulas/aula115.py
@@ -2,3 +2,14 @@
 Problemas dos parametros mutaveis em funções python
 """
 
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def muda_lista(*args):
+    return [x*2 for x in args]
+
+def test(x):
+    if x > 5:
+        return x
+
+teste = list(filter(test, lista))
+print(teste)
