4e793d34799899609464401c6e2366d0e3cf9989
Diego Santos <dieboas88@gmail.com>
2023-07-27 10:46:53 -0300

    seção 3

--- secao_3/aulas/aula134.py
+++ secao_3/aulas/aula134.py
@@ -1,3 +1,16 @@
+"""
+Relações entre classes: associação, agregação e composição
+Associação é um tipo de relação onde os objetos
+estão ligados dentro do sistema.
+Essa é a relação mais comum entre objetos e tem subconjuntos
+como agregação e composição (que veremos depois).
+Geralmente, temos uma associação quando um objeto tem
+um atributo que referencia outro objeto.
+A associação não especifica como um objeto controla
+o ciclo de vida de outro objeto.
+"""
+
+
 class Escritor:
     def __init__(self, nome):
         self.nome = nome
@@ -17,8 +30,15 @@ class FerramentaEscrever:
         self.nome = nome
 
     def escrever(self):
-        return f'{self.nome} está escrevendo'
+        return f'{self.nome} para escrever'
 
 
 escritor = Escritor('Santz')
-lapis = ...
+escritor2 = Escritor('Cauã')
+lapis = FerramentaEscrever('Lapis preto')
+caneta = FerramentaEscrever('Caneta Vermelha')
+escritor.ferramenta = caneta
+escritor2.ferramenta = lapis
+
+print(f'{escritor2.nome} está usando {escritor.ferramenta.escrever()}')
+print(f'{escritor.nome} está usando {escritor.ferramenta.escrever()}')
---
+++ secao_3/aulas/aula135.py
@@ -0,0 +1,41 @@
+"""
+Relações entre classes: associação, agregação e composição
+Agregação é uma forma mais especializada de associação
+entre dois ou mais objetos. Cada objeto terá
+seu ciclo de vida independente.
+Geralmente é uma relação de um para muitos, onde um
+objeto tem um ou muitos objetos.
+Os objetos podem viver separadamente, mas pode
+se tratar de uma relação onde um objeto precisa de
+outro para fazer determinada tarefa.
+(existem controvérsias sobre as definições de agregação).
+"""
+
+
+class Carrinho:
+    def __init__(self):
+        self._produtos = []
+        
+    def total(self):
+        return sum([p.valor for p in self._produtos])
+    
+    def inserir_produto(self, *produtos):
+        self._produtos.extend(produtos)
+    
+    def listar_produtos(self):
+        for produto in self._produtos:
+            print(produto.nome, produto.valor)
+    
+    
+
+class Produto:
+    def __init__(self, nome, valor):
+        self.nome = nome
+        self.valor = valor
+        
+        
+carrinho = Carrinho()
+p1, p2 = Produto('morango', 21), Produto('Queijo', 42)
+carrinho.inserir_produto(p1, p2)
+carrinho.total()
+print(carrinho.total())
