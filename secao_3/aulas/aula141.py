fdc7144054b070728578f91c2790d9e6ab60c782
Diego Santos <dieboas88@gmail.com>
2023-08-09 11:33:40 -0300

    seção 3

--- secao_3/aulas/aula141.py
+++ secao_3/aulas/aula141.py
@@ -20,21 +20,54 @@ from abc import ABC, abstractmethod
 
 
 # class Log(metaclass=ABCMeta)
-class Log(ABC):
+# class Log(ABC):
     
-    @abstractmethod
-    def _log(self, msg): ...
+#     @abstractmethod
+#     def _log(self, msg): ...
     
-    def log_error(self, msg):
-        return self._log(f'error: {msg}')
+#     def log_error(self, msg):
+#         return self._log(f'error: {msg}')
 
-    def log_success(self, msg):
-        return self._log(f'success: {msg}')
+#     def log_success(self, msg):
+#         return self._log(f'success: {msg}')
     
-class LogPrintMixin(Log):
-    def _log(self, msg):
-        print(f'{msg} | {self.__class__.__name__}')
+# class LogPrintMixin(Log):
+#     def _log(self, msg):
+#         print(f'{msg} | {self.__class__.__name__}')
         
         
-l = LogPrintMixin()
-l.log_success('hello')
+# l = LogPrintMixin()
+# l.log_success('hello')
+
+"""
+1. Classe Abstrata e Métodos Abstratos:
+
+Uma classe abstrata define um contrato básico que suas subclasses devem cumprir. Ela pode conter métodos abstratos, que são métodos declarados, mas não implementados na classe abstrata. As subclasses são obrigadas a implementar esses métodos.
+"""
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def area(self):
+        return round(3.14 * self.radius ** 2, 2)
+    
+    
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+        
+    def area(self):
+        return round(self.side ** 2, 2)
+    
+circulo = Circle(8)
+print(circulo.area())
+
+quadrado = Square(14)
+print(quadrado.area())
