6139887ad36fcf834e46d1117c3b50b78923683f
Diego Santos <dieboas88@gmail.com>
2023-11-26 11:26:51 -0300

    seção 3

--- secao_3/exercicios/ex24_b.py
+++ secao_3/exercicios/ex24_b.py
@@ -2,7 +2,7 @@ from ex24_a import Pessoa
 import json
 
 
-with open("secao_3/exercicios/ex24.json", "r") as file:
+with open("secao_3/exercicios/ex24.json", " r ") as file:
     data = json.load(file)
     p1 = Pessoa(**data[0])
     p2 = Pessoa(**data[1])
